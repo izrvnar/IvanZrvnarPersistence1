bc21094254c2e236f56864d82e6d02cb269d406e
Ivan Zrvnar <izrvnar@gmail,com>
2022-02-23 22:03:57 -0500

    added the functionality to users can pick an image and save to the disk but still need to create a custom class in order to complete this functionality

--- IvanZrvnarPersistence1/ViewController.swift
+++ IvanZrvnarPersistence1/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 // changing the
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     
 //MARK: -View did Load
     override func viewDidLoad() {
         super.viewDidLoad()
+        // adding a button to request photo acsess
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
 
     }//: View did load
 
@@ -30,6 +32,34 @@ class ViewController: UICollectionViewController {
             fatalError("Unable to deque person cell")
         }
         return cell
+    }
+    
+    // adding new person fucntion and conforming to UI image picker
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    // image picker method
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // typecasting as an image
+        guard let image = info[.editedImage] as? UIImage else {return}
+        
+        // compressing the image quality and creating a UUID object 
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+                try? jpegData.write(to: imagePath)
+            }
+
+            dismiss(animated: true)
+    }
+        // finding a path to the photos
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
     
     
