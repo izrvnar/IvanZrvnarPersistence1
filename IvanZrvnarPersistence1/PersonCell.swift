ae708ea0d20d595d4eea1d7d79b7a23809ed4b77
Ivan Zrvnar <izrvnar@gmail,com>
2022-02-23 21:09:13 -0500

    changed the view controller to a collection view controller, created a custom class that represents a custom cell and connected the outlets to its own file

---
+++ IvanZrvnarPersistence1/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  IvanZrvnarPersistence1
+//
+//  Created by Ivan Zrvnar on 2022-02-23.
+//
+
+import UIKit
+
+// creating a custom class for our cell
+class PersonCell: UICollectionViewCell {
+    
+    //MARK: - OUTLETS
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
